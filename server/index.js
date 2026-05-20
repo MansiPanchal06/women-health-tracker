db1c7a43752cac0fc0f615297bd9c2f123e7a035
Mansi Panchal <mansipanchall0606@gmail.com>
2026-05-21 00:52:11 +0530

    Fixed CORS issue

--- server/index.js
+++ server/index.js
@@ -12,10 +12,7 @@ import blogRoutes from './routes/blogRoutes.js';
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
